82e459e0aafa660af2e143e145313868cd56fbdb
Matheus D. Gomes Chichura <matheusdgo@gmail.com>
2024-11-28 22:08:00 -0300

    Resolvido conflito no arquivo commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,5 +15,29 @@ Cypress.Commands.add('submitForm', () => {
 })
 
 Cypress.Commands.add('validarMensagemSubmitForm', (msg) => {
-    cy.get('div[role="alert"]').contains(msg).should("be.visible")
+    cy.get('div[role="alert"]')
+        .should('be.visible')
+        .invoke('text')
+        .should('include', msg)
+})
+
+Cypress.Commands.add('criarMovimentacao', (movimentacao) => {
+    cy.get('a[href="/movimentacao"]').click();
+    cy.get('#tipo').type(movimentacao.tipo);
+    cy.get('input[name="data_transacao"]').type(movimentacao.data_transacao);
+    cy.get('input[name="data_pagamento"]').type(movimentacao.data_pagamento);
+    cy.get('input[name="descricao"]').type(movimentacao.descricao);
+    cy.get('input[name="interessado"]').type(movimentacao.interessado);
+    cy.get('input[name="valor"]').type(movimentacao.valor);
+    cy.get('select[name="conta"]').select(movimentacao.conta);
+    cy.get('input[name="status"][value="1"]').click();
+    cy.get('button[type="submit"]').click();
+    cy.contains('Movimentação adicionada com sucesso!').should('be.visible');
+})
+
+Cypress.Commands.add('visitarResumoMensal', (mes, ano) => {
+    cy.get('a[href="/extrato"]').contains('Resumo Mensal').click();
+    cy.get('select[name="mes"]').select(mes);
+    cy.get('select[name="ano"]').select(ano);
+    cy.get('.btn-primary').contains('Buscar').click();
 })
